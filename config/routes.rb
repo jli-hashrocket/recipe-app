d5aa8c149fb82f6873550fecb731051cd7ee4aa9
macheteshuffle <jeff.li029@gmail.com>
2015-10-29 22:07:41 -0400

    added create/destroy/update actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'recipes/index'
 
   root 'home#index'
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :create, :update, :destroy]
 end
